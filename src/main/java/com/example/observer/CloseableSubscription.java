b4e519f4546513ac539cc0a9bcd0390ba43f6373
1strebel <firstrebel@ya.ru>
2022-08-22 22:04:37 +0300

    Add observable

---
+++ src/main/java/com/example/observer/CloseableSubscription.java
@@ -0,0 +1,96 @@
+package com.example.observer;
+
+import java.util.*;
+import java.util.function.Consumer;
+
+class Event<TArgs> {
+    private int count = 0;
+    private Map<Integer, Consumer<TArgs>>
+            handlers = new HashMap<>();
+
+    public Subscription addHandler(Consumer<TArgs> handler) {
+        int i = count;
+        handlers.put(count++, handler);
+        return new Subscription(this, i);
+    }
+
+    public void fire(TArgs args) {
+        for (Consumer<TArgs> handler : handlers.values())
+            handler.accept(args);
+    }
+
+    public class Subscription implements AutoCloseable {
+        private Event<TArgs> event;
+        private int id;
+
+        public Subscription(Event<TArgs> event, int id) {
+            this.event = event;
+            this.id = id;
+        }
+
+        @Override
+        public void close() /*throws Exception*/ {
+            event.handlers.remove(id);
+        }
+    }
+}
+
+class ChangePropertyEventArgs {
+    public Object source;
+    public String propertyName;
+
+    public ChangePropertyEventArgs(Object source, String propertyName) {
+        this.source = source;
+        this.propertyName = propertyName;
+    }
+}
+
+class Person {
+    public Event<ChangePropertyEventArgs>
+            propertyChanged = new Event<>();
+
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (this.age == age) return;
+
+        boolean oldCanVote = getCanVote();
+
+        this.age = age;
+        propertyChanged.fire(new ChangePropertyEventArgs(
+                this, "age"
+        ));
+
+        if (oldCanVote != getCanVote()) {
+            propertyChanged.fire(new ChangePropertyEventArgs(
+                    this, "canVote"
+            ));
+        }
+    }
+
+    public boolean getCanVote() {
+        return age >= 18;
+    }
+}
+
+class HandmadeEventsDemo {
+    public static void main(String[] args) {
+        Person person = new Person();
+        Event<ChangePropertyEventArgs>.Subscription sub =
+                person.propertyChanged.addHandler(changePropertyEventArgs -> {
+                    System.out.println("Person's "
+                            + changePropertyEventArgs.propertyName + " has changed");
+                });
+        person.setAge(17);
+        person.setAge(18);
+        sub.close();
+        person.setAge(19);
+    }
+}
+
+public class CloseableSubscription {
+}
